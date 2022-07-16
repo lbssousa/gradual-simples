5141d9f8d60f0ed1c0aca10e852b55eaf16dfe85
Laércio de Sousa <lbsousajr@gmail.com>
2022-07-16 17:34:59 -0300

    Atualizados os workflows do GitHub Actions para os nomos padrões de nome dos arquivos principais.

--- .github/actions/lualatex/entrypoint.sh
+++ .github/actions/lualatex/entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-dirName=${1}
-fileName=${2}
+base_name=${1}
+layout_suffix=${2}
+file_name=${base_name}-${layout_suffix}
 
 echo ">>> Creating output directory ${OUT_DIR}..."
 mkdir --parent ${OUT_DIR}
 
-echo ">>> Building PDF document for file ${dirName}/${fileName}..."
-latexmk --lualatex --synctex=1 --interaction=nonstopmode --file-line-error --halt-on-error ${dirName}/${fileName}
+echo ">>> Building PDF document for file ${file_name}..."
+latexmk --lualatex --synctex=1 --interaction=nonstopmode --file-line-error --halt-on-error ${file_name}
 
-echo ">>> Copying ${fileName} output files to ${OUT_DIR}"
-cp ${fileName}.* ${OUT_DIR}
+echo ">>> Copying ${file_name} output files to ${OUT_DIR}"
+cp ${file_name}.* ${OUT_DIR}
